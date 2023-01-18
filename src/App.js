01128ec22ae4c5f58cf201b0a8d8238b11dde874
sergio13v <56689687+sergio13v@users.noreply.github.com>
2023-01-18 20:00:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var nombre = "Sergio";
+  var saludo = <p>
+    Hola mundo soy {nombre}<br/>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {saludo}
         <a
           className="App-link"
           href="https://reactjs.org"
