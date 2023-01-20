7fc01b05f27b5d7382c523caa5f0eff37e77366a
sergio13v <56689687+sergio13v@users.noreply.github.com>
2023-01-20 14:20:13 +0100

    App finalizada
    Generador de memes finalizado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
 import './App.css';
 
 function App() {
 
-  var nombre = "Sergio";
-  var saludo = <p>
-    Hola mundo soy {nombre}<br/>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>;
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+
+  const onClickExportar = function(evento){
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement("a");
+      link.download = "meme.png";
+      link.href = img;
+      link.click();
+  });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {saludo}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value ="fire">Casa en llamas</option>
+        <option value ="futurama">Futurama</option>
+        <option value ="history">History channel</option>
+        <option value ="matrix">Matrix</option>
+        <option value ="philosoraptor">Philosoraptor</option>
+        <option value ="smart">Smart guy</option>
+      </select><br/>
+
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1"/><br/>
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2"/><br/>
+
+      <button onClick={onClickExportar}>Exportar imagen</button>
+
+      <div className="meme" id="meme">
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span><br/>
+        <img alt="meme" src={"/img/" + imagen + ".jpg"}/>
+      </div>
     </div>
   );
 }
